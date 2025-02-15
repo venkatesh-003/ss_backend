3181fe9e158d308591169303512b456183a882cd
venkatesh <pothamsettyvenky003@gmail.com>
2025-02-15 15:59:49 -0800

    crum68#: connected Database using MongoDB  commit message

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(()=> console.log("Connected to MongoDb"))
+.catch((err)=> console.error("MongoDB connection error",err))
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email:{type:String, unique:true},
+    password: String,
+});
+
+const User = mongoose.model("User",userSchema);
+
+const JWT_SECRET = crypto.randomBytes(32).toString("hex");
+console.log("Generated JWT Secret Key:", JWT_SECRET);
+
+
+const users = [];
+
+
+app.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;
+try{
+    const existing = await User.findOne({email});
+    if(existing){
+        return res.status(400).json({ error: "Email already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({username,email,password:hashedPassword});
+    await newUser.save();
+
+    // users.push({ username, email, password: hashedPassword });
+
+    const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: "1h" });
+    res.json({ message: "User registered successfully!", token });
+}catch(error){
+    res.status(500).json({error:"server error"});
+}
+   
+
+    
+});
+
+// Login Route
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+try{
+    const user = await User.findOne({email});
+    if(!user) return res.status(404).json({error:"User not found"});
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ error: "Invalid credentials" });
+
+    // Generate a new token for each login attempt
+    const token = jwt.sign({ email: user.email }, JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ message: "Login successful", token });
+    
+}catch(error){
+res.status(500).json({error:"server error"});
+}
+      // Send the new token in the response
+});
+
+// Start Server
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
